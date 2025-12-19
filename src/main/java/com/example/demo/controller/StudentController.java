121404d03c628bbd0d54824753bbdaf86e3ec5d3
bala-amypo <bala@amypo.in>
2025-12-19 09:08:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -33,7 +33,7 @@ public class StudentController {
 
     @PutMapping("/updatedata/{id}")
     public Student updatedStudent(@PathVariable Long id, @Valid @RequestBody Student student){
-    return studentService.updatedata(id,student);
+    return StudentService.updatedata(id,student);
     }
     
     @DeleteMapping("/deletedata/{id}")
